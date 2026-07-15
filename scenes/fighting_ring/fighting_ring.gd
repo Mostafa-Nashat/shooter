7b59bf3a3cc8163d3088a4d8db35078528e0b0a3
Mostafa-Nashat <Mostafa-Nashat@proton.me>
2026-07-15 19:27:39 +0300

    bug fix

--- scenes/fighting_ring/fighting_ring.gd
+++ scenes/fighting_ring/fighting_ring.gd
@@ -13,8 +13,12 @@ var enemy_count := 0
 var enemy: PackedScene = preload("res://scenes/enemy/enemy.tscn")
 
 func spawner_logic(spawner: Area2D) -> bool:
+	await wait_physics_frame(1)
 	for area in spawner.get_overlapping_areas():
 		if area.get_collision_layer_value(4):
+			return false
+	for body in spawner.get_overlapping_bodies():
+		if body is PhysicsBody2D:
 			return false
 	return true
 	
@@ -39,16 +43,19 @@ func spawn_enemy_attempt(number: int) -> void:
 	var valid_spawners: Array[Area2D]
 	for node in spawners_parent:
 		var spawner: Area2D = node
-		var valid: bool = spawner_logic(spawner)
+		var valid: bool = await spawner_logic(spawner)
 		if valid:
 			valid_spawners.append(spawner)
 	if len(valid_spawners) == 0:
 		return
+	print("number: ", number)
+	print("valid_spawners:", len(valid_spawners))
 	if number > len(valid_spawners):
 		for spawner in valid_spawners:
 			spawn_enemy(spawner.position)
 			await get_tree().create_timer(spawn_delay).timeout
 			spawn_enemy_attempt(number - len(valid_spawners)) 
+		return
 	for index in range(number):
 		spawn_enemy(valid_spawners[index].position)
 
