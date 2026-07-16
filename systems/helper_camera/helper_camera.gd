06c6073d5f97470e08059e76fa88635e8321cc04
Mostafa-Nashat <Mostafa-Nashat@proton.me>
2026-07-16 08:45:23 +0300

    set resolution and removed stretch for better development workflow

--- systems/helper_camera/helper_camera.gd
+++ systems/helper_camera/helper_camera.gd
@@ -1,17 +1,13 @@
 class_name HelperCamera2D
 extends Camera2D
 
-# this camera just helps me know what the player sees in the editor
-# since I have my game strechec so the normal camera is 4 times too big
-
-@export var intended_zoom: Vector2 = Vector2(1, 1)
 @export var hitbox: Area2D 
 
 func _ready() -> void:
-	zoom = intended_zoom
 	var rect := RectangleShape2D.new()
-	var stretch_scale: int = ProjectSettings.get_setting("display/window/stretch/scale")
-	rect.size = DisplayServer.screen_get_size()  / stretch_scale
+	rect.size.x = ProjectSettings.get_setting("display/window/size/viewport_width")
+	rect.size.y = ProjectSettings.get_setting("display/window/size/viewport_height")
+	print(rect.size)
 	var shape = CollisionShape2D.new()
 	shape.shape = rect
 	hitbox.add_child(shape)
