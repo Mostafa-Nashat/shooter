727ebe827efdda3d717c4a2842d1f89b6ff11827
Mostafa-Nashat <Mostafa-Nashat@proton.me>
2026-07-16 16:42:38 +0300

    added dash fading affect

--- scenes/player/dashing.gd
+++ scenes/player/dashing.gd
@@ -5,6 +5,7 @@ var direction: Vector2
 
 @export var SPEED_MULTIPLYER: float = 2
 @export var timer: Timer
+@export var fade_affect: GPUParticles2D
 
 
 func _start():
@@ -14,12 +15,14 @@ func _start():
 		direction = Vector2.LEFT
 	timer.start()
 	player.velocity = direction * player.SPEED * SPEED_MULTIPLYER
-
+	fade_affect.emitting = true
+	
 func _update():
 	player.arm.look_at_target(player.get_global_mouse_position(), player.MAX_HAND_SWING_SPEED)
 	player.allow_weapon_control()
 	player.show_hud_elements()
 	if timer.is_stopped():
+		fade_affect.emitting = false
 		state.switch_state("ground")
 		return
 	
