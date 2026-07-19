9df1a8f3ba39c984fde142e66c35b330806aba05
Mostafa-Nashat <Mostafa-Nashat@proton.me>
2026-07-19 09:12:50 +0300

    removed test logs

--- systems/enemy/SpawnComposer.gd
+++ systems/enemy/SpawnComposer.gd
@@ -26,20 +26,15 @@ func spawn_off_camera(
 	number: int,
 	closest_to: Node2D
 	):
-	print(number)
 	var valid_spawners := spawners.filter(_spawner_off_camera)
 	valid_spawners.sort_custom(_sort_by_closest.bind(closest_to))
 	if number > len(valid_spawners):
-		print("recurse")
 		for spawner in valid_spawners:
 			spawner.spawn(parent, enemy)
-			print(spawner)
 		await get_tree().create_timer(recursive_delay).timeout
 		var enemies_left := number - len(valid_spawners)
 		spawn_off_camera(parent, enemy, enemies_left, closest_to)
 	else:
-		print("normal")
 		for index in number:
 			var spawner: Enemy_Spawner = valid_spawners[index]
-			print(spawner)
 			spawner.spawn(parent, enemy)
