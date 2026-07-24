8b5cad580040aede13a5051b38c6f13d8a5e67ef
Mostafa-Nashat <Mostafa-Nashat@proton.me>
2026-07-24 21:32:58 +0300

    added method for recipe resource

--- systems/item/Recipe.gd
+++ systems/item/Recipe.gd
@@ -6,3 +6,16 @@ var description: String
 var ingredients: Array[InventoryItemData]
 var preview: Texture2D
 var result: Result
+
+func craftable(player: Player) -> bool:
+	for ingredient in ingredients:
+		var is_craftable: bool = false
+		for item in player.item_taker.inventory:
+			if ingredient.data.name == item.data.name:
+				is_craftable = true
+		if !is_craftable:
+			return false
+	return true
+
+func apply(player: Player):
+	result.apply(player)
