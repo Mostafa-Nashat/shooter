45c16c3f7e7b8ee2b2c3123157097b12714fb7fd
Mostafa-Nashat <Mostafa-Nashat@proton.me>
2026-07-25 12:00:13 +0300

    added recipeListItem ui scene

---
+++ scenes/player/inventory/recipe.gd
@@ -0,0 +1,33 @@
+class_name RecipeListItem
+extends HBoxContainer
+
+@export var data: Recipe
+
+@export var player: Player
+@export var preview_texture: TextureRect
+@export var title: Label
+@export var description: Label
+@export var needs: Label
+@export var button: Button
+
+func _ready() -> void:
+	preview_texture.texture = data.preview
+	title.text = data.title
+	description.text = data.description
+	needs.text = "ingredients:"
+	for ingredient in data.ingredients:
+		needs.text += " " + ingredient.data.name
+	if !data.craftable(player):
+		button.disabled = true
+	button.text = "craft"
+	button.button_up.connect(apply)
+
+func apply() -> void:
+	data.apply(player)
+
+
+func _process(_delta: float) -> void:
+	if !data.craftable(player):
+		button.disabled = true
+	else:
+		button.disabled = false
