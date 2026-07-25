d205210aa4c5063ac176897516eabb82c9328964
Mostafa-Nashat <Mostafa-Nashat@proton.me>
2026-07-25 10:42:41 +0300

    made  item count part of validation + made apply function remove items

--- systems/item/Recipe.gd
+++ systems/item/Recipe.gd
@@ -11,11 +11,15 @@ func craftable(player: Player) -> bool:
 	for ingredient in ingredients:
 		var is_craftable: bool = false
 		for item in player.item_taker.inventory:
-			if ingredient.data.name == item.data.name:
+			if ingredient.data.name == item.data.name and ingredient.count <= item.count:
 				is_craftable = true
 		if !is_craftable:
 			return false
 	return true
 
 func apply(player: Player):
+	for ingredient in ingredients:
+		for item in player.item_taker.inventory:
+			if ingredient.data.name == item.data.name:
+				item.count -= ingredient.count
 	result.apply(player)
