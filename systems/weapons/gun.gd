53348c0e1ad4bc7e159c34ffdf078ac7fe502469
Mostafa-Nashat <Mostafa-Nashat@proton.me>
2026-07-26 01:20:25 +0300

    added better gun data hotswapping

--- systems/weapons/gun.gd
+++ systems/weapons/gun.gd
@@ -6,15 +6,17 @@ var base_bullet = Bullet.new()
 
 func _setup() -> void:
 	add_sprite(weapon.sprite, weapon.offset)
-	
-func _use(_user: Node) -> void:
 
+func update():
+	add_sprite(gun.sprite, gun.offset)
+
+func _use(_user: Node) -> void:
 	for index: float in gun.count:
 		var bullet: Bullet = base_bullet.duplicate()
 		bullet.set_collision_mask_value(2, true)
 		bullet.bullet = weapon.bullet
 		bullet.position = to_global(position + weapon.bullet.offset)
-		var offset := lerpf(-gun.spread, gun.spread, index/(gun.count - 1))
+		var offset := lerpf(-gun.spread, gun.spread, (index+1)/gun.count)
 		bullet.rotation = global_rotation + deg_to_rad(offset)
 		get_tree().root.add_child(bullet)
 	
