c91ebf949e83498dd9841170971a73be6bf15688
Mostafa-Nashat <Mostafa-Nashat@proton.me>
2026-07-27 19:42:48 +0300

    created ItemDroper

---
+++ systems/item/ItemDroper.gd
@@ -0,0 +1,23 @@
+class_name ItemDroper
+extends Node2D
+
+var base_item := preload("res://systems/item/base_item.tscn")
+
+func drop(item: ItemData, count: int, direction: Vector2, speed: float) -> void:
+	var item_node: Item = base_item.instantiate()
+	item_node.data = item
+	item_node.count = count
+	item_node.global_position = global_position
+	item_node.linear_velocity = direction.normalized() * speed
+	var root := get_tree().root
+	root.add_child(item_node)
+
+func knock_from(item: ItemData, count: int , from: Node2D, speed: float) -> void:
+	var item_node: Item = base_item.instantiate()
+	item_node.data = item
+	item_node.count = count
+	item_node.global_position = global_position
+	var direction := -to_local(from.global_position).normalized()
+	item_node.linear_velocity = direction * speed
+	var root := get_tree().root
+	root.add_child(item_node)
