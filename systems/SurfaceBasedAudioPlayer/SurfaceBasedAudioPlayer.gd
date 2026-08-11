c5373dbc5764eff169fad34a9fcade15da7d39ad
Mostafa-Nashat <Mostafa-Nashat@proton.me>
2026-08-11 11:42:24 +0300

    gave surface based audio player abbility to play audio

--- systems/SurfaceBasedAudioPlayer/SurfaceBasedAudioPlayer.gd
+++ systems/SurfaceBasedAudioPlayer/SurfaceBasedAudioPlayer.gd
@@ -2,22 +2,34 @@ class_name SurfaceBasedAudioPlayer
 extends RefCounted
 
 var path: String
+var player: AudioStreamPlayer2D
 
-static func create(audio_path: String) -> SurfaceBasedAudioPlayer:
-	var audio_player := SurfaceBasedAudioPlayer.new()
-	audio_player.path = audio_path
-	return audio_player
+static func create(audio_path: String, audio_player: AudioStreamPlayer2D) -> SurfaceBasedAudioPlayer:
+	var surface_audio_player := SurfaceBasedAudioPlayer.new()
+	surface_audio_player.path = audio_path
+	surface_audio_player.player = audio_player
+	return surface_audio_player
 
 func get_surface_audio(tilemap: TileMapLayer, global_position: Vector2, data_name: String) -> AudioStream:
 	var local_position := tilemap.to_local(global_position)
 	var map_position := tilemap.local_to_map(local_position)
 	var cell_data := tilemap.get_cell_tile_data(map_position)
+	if !cell_data:
+		print("cell data isnt present")
+		return
 	if !cell_data.has_custom_data(data_name):
 		print("cell does not have requested audio data")
 		return
 	var audio_data: String = cell_data.get_custom_data(data_name)
 	var audio := _get_random_audio_from_directory(audio_data)
 	return audio
+
+func play_surface_audio(tilemap: TileMapLayer, global_position: Vector2, data_name: String) -> void:
+	var audio := get_surface_audio(tilemap, global_position, data_name)
+	if player.playing:
+		return
+	player.stream = audio
+	player.play()
 
 func _get_random_audio_from_directory(directory_name: String) -> AudioStream:
 	var audio_directory_path := path + directory_name
